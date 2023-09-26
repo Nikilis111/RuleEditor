727a70c3d0ba17540b2fb576db8c8a57b2f035d9
Nikita <Nikitagurin71@gmail.com>
2023-09-26 16:05:13 +0300

    Added MUI to project

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,17 @@
 import Header from "./Header";
 import Menu from "./Menu";
+import Button from '@mui/material/Button';
+import DeleteForeverIcon from '@mui/icons-material/DeleteForever';
 
 function App() {
   return (
    <>
     <Header />
     <Menu />
+    <Button variant="text">Text</Button>
+    <Button variant="contained">Contained</Button>
+    <Button variant="outlined">Outlined</Button>
+    <DeleteForeverIcon />
    </>
   );
 }
